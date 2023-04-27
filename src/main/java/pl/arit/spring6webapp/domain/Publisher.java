f901cd0fa2e193bfe460514bf1002146a8ded2a1
Andrzej Rawski <rawski.andrzej@gmail.com>
2023-04-27 09:00:06 +0200

    Publisher repository and data in bootstrap

--- src/main/java/pl/arit/spring6webapp/domain/Publisher.java
+++ src/main/java/pl/arit/spring6webapp/domain/Publisher.java
@@ -1,9 +1,11 @@
 package pl.arit.spring6webapp.domain;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+@Entity
 public class Publisher {
 
     @Id
