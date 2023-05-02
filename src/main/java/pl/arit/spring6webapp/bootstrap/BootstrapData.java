c75d46f81c51f60cb3de07dceddb1a0e0a0d958d
Andrzej Rawski <rawski.andrzej@gmail.com>
2023-05-02 21:53:44 +0200

    Publisher relationship

--- src/main/java/pl/arit/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/pl/arit/spring6webapp/bootstrap/BootstrapData.java
@@ -40,28 +40,32 @@ public class BootstrapData implements CommandLineRunner {
         rod.setFirstName("Rod");
         rod.setLastName("Johnson");
 
-        Book noEBJ = new Book();
-        noEBJ.setTitle("J2EE Development without EJB");
-        noEBJ.setIsbn("567890");
+        Book noEJB = new Book();
+        noEJB.setTitle("J2EE Development without EJB");
+        noEJB.setIsbn("567890");
 
         Author rodSaved = authorRepository.save(rod);
-        Book noEJBSaved = bookRepository.save(noEBJ);
+        Book noEJBSaved = bookRepository.save(noEJB);
 
         ericSaved.getBooks().add(dddSaved);
         rodSaved.getBooks().add(noEJBSaved);
 
+        Publisher publisher = new Publisher();
+        publisher.setPublisherName("My Publisher");
+        publisher.setAddress("123 Main");
+        Publisher savedPublisher = publisherRepository.save(publisher);
+
+        dddSaved.setPublisher(savedPublisher);
+        noEJB.setPublisher(savedPublisher);
+
         authorRepository.save(ericSaved);
         authorRepository.save(rodSaved);
+        bookRepository.save(dddSaved);
+        bookRepository.save(noEJBSaved);
 
         System.out.println("In Bootstrap");
         System.out.println("Author Count: " + authorRepository.count());
         System.out.println("Book Count: " + bookRepository.count());
-
-        Publisher publisher = new Publisher();
-        publisher.setPublisherName("My Publisher");
-        publisher.setAddress("123 Main");
-        publisherRepository.save(publisher);
-
         System.out.println("Publisher Count: " + publisherRepository.count());
     }
 }
